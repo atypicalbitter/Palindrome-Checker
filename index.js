571b779a9a8fe1519dbea2110fabaf9304df2af2
Matt Dudman <mattdudman95@googlemail.com>
2024-07-18 16:48:47 +0100

    Added more styling, animation and JS.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const input = document.getElementById()
+    const input = document.getElementById('input'); 
+
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+    
+    function check() {
+        const value = input.value.trim();
+        if (value === "") {
+            alert("Please add your text to the box below");
+        } else {
+            const reverse = reverseString(value);
+            if (value === reverse) {
+                alert("This is a Palindrome!");
+            } else {
+                alert("This is not a Palindrome");
+            }
+        }
+        input.value = "";
+    }
+    
+
